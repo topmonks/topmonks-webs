814899c95b982573b01a655c83f3a0ebf6c975b8
Aleš Roubíček <ales@roubicek.name>
2018-12-17 08:08:23 +0100

    WIP Pulumi Outputs

--- index.ts
+++ index.ts
@@ -4,9 +4,10 @@ import * as topmonks from "./.scripts/pulumi-resources/website";
 const websites = require("./websites.json");
 
 export const sites: any = {};
-for (const domain of Object.keys(websites)) {
+for (const domain in websites) {
   const website = topmonks.WebSite.create(domain, websites[domain]);
   sites[domain] = {
+    url: website.url,
     contentBucketUri: website.contentBucketUri,
     cloudFrontId: website.cloudFrontId
   };
