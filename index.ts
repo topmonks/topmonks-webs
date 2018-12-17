93e61219b165e520b54470bb3d8542b134b8a24d
Aleš Roubíček <ales@roubicek.name>
2018-12-17 08:02:43 +0100

    WIP Pulumi Outputs

--- index.ts
+++ index.ts
@@ -3,12 +3,11 @@ import * as topmonks from "./.scripts/pulumi-resources/website";
 
 const websites = require("./websites.json");
 
-export const sites = Object.keys(websites)
-  .map(domain => topmonks.WebSite.create(domain, websites[domain]))
-  .map(site => ({
-    [`${site.domain}`]: {
-      contentBucketUri: site.contentBucketUri,
-      cloudFrontId: site.cloudFrontId
-    }
-  }))
-  .reduce((acc, site) => Object.assign(acc, site), {});
+export const sites: any = {};
+for (const domain of Object.keys(websites)) {
+  const website = topmonks.WebSite.create(domain, websites[domain]);
+  sites[domain] = {
+    contentBucketUri: website.contentBucketUri,
+    cloudFrontId: website.cloudFrontId
+  };
+}
