567dd0b7face01b54b6b95d65c08cc2ea32618f9
Aleš Roubíček <ales@roubicek.name>
2019-07-30 07:08:15 +0200

    Guard for NPE

--- __tests__/lighthouse-results.js
+++ __tests__/lighthouse-results.js
@@ -27,8 +27,9 @@ test("Lighthouse SEO", assertThreshold(results.categories.seo, 0.75));
 test("Lighthouse Vulnerable libraries", t =>
   t.true(
     results.audits["no-vulnerable-libraries"].rawValue,
-    "Contains vulnerable libraries: " +
-      results.audits["no-vulnerable-libraries"].extendedInfo.jsLibs
-        .map(x => x.name + "@" + x.version)
-        .join()
+    results.audits["no-vulnerable-libraries"].extendedInfo &&
+      "Contains vulnerable libraries: " +
+        results.audits["no-vulnerable-libraries"].extendedInfo.jsLibs
+          .map(x => x.name + "@" + x.version)
+          .join()
   ));
