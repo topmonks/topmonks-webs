834b0a681f10e5632af33803d3558a036356f12c
Cihlova <33567545+Cihlova@users.noreply.github.com>
2019-08-26 11:16:21 +0200

    New Website Monkslab

---
+++ monkslab.cz/config/task-config.js
@@ -0,0 +1,52 @@
+const globImporter = require("node-sass-glob-importer");
+const createSharedTaskConfig = require("../../shared/config/createSharedTaskConfig");
+
+const config = createSharedTaskConfig(__dirname, {
+  images: true,
+  fonts: true,
+  static: true,
+  svgSprite: true,
+
+  javascripts: {
+    entry: {
+      app: ["./index.js"]
+    }
+  },
+
+  stylesheets: {
+    sass: {
+      importer: globImporter()
+    }
+  },
+
+  html: {
+    dataFile: "../data/global.json",
+    htmlmin: {
+      collapseBooleanAttributes: true,
+      decodeEntities: true,
+      minifyCSS: true,
+      minifyJS: true,
+      removeAttributeQuotes: true,
+      removeOptionalTags: true,
+      removeRedundantAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkTypeAttributes: true
+    }
+  },
+
+  browserSync: {
+    server: {
+      // should match `dest` in
+      // path-config.json
+      baseDir: "./public/monkslab.cz"
+    }
+  },
+
+  production: {
+    rev: true
+  }
+});
+
+module.exports = config;
+
+// module.exports = createSharedTaskConfig(__dirname, config); // <- Use if you want to enable access to shared assets
