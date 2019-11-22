7eb337116eb00f8d2533f87fecc81572abf85055
Aleš Roubíček <ales@roubicek.name>
2019-11-22 10:43:11 +0100

    [www.hlidacshopu.cz] Simplified media articles

--- www.hlidacshopu.cz/config/task-config.js
+++ www.hlidacshopu.cz/config/task-config.js
@@ -1,5 +1,19 @@
+const fs = require("fs").promises;
 const globImporter = require("node-sass-glob-importer");
+const markdownToJSON = require("gulp-markdown-to-json");
+const marked = require("marked");
+const merge = require("gulp-merge-json");
+const path = require("path");
+const pathConfig = require("./path-config.json");
+const projectPath = require("@topmonks/blendid/gulpfile.js/lib/projectPath");
 const createSharedTaskConfig = require("../../shared/config/createSharedTaskConfig");
+
+const dataFile = name => projectPath(pathConfig.src, `data/${name}.json`);
+const jsonData = n =>
+  fs
+    .readFile(dataFile(n), "utf8")
+    .then(f => JSON.parse(f))
+    .catch(() => {});
 
 const config = createSharedTaskConfig(__dirname, {
   images: true,
@@ -31,6 +45,27 @@ const config = createSharedTaskConfig(__dirname, {
       removeRedundantAttributes: true,
       removeScriptTypeAttributes: true,
       removeStyleLinkTypeAttributes: true
+    },
+    nunjucksRender: {
+      manageEnv(env) {
+        env.addFilter("longDate", str =>
+          new Date(Date.parse(str)).toLocaleString("cs", {
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+          })
+        );
+        env.addFilter("split", (str, seperator) => str.split(seperator));
+        env.addFilter(
+          "transformation",
+          (s, t) => s && s.replace("/upload/", `/upload/${t}/`)
+        );
+      }
+    },
+    dataFunction(_, cb) {
+      Promise.all([jsonData("media"), jsonData("mediaImages")]).then(
+        ([media, mediaImages]) => cb(null, { media, mediaImages })
+      );
     }
   },
 
@@ -39,6 +74,38 @@ const config = createSharedTaskConfig(__dirname, {
       // should match `dest` in
       // path-config.json
       baseDir: "./public/www.hlidacshopu.cz"
+    }
+  },
+
+  additionalTasks: {
+    initialize({ task, src, dest, series, watch }, PATH_CONFIG, TASK_CONFIG) {
+      const dataPath = projectPath(PATH_CONFIG.src, "data");
+      const mediaSrc = projectPath(PATH_CONFIG.src, "data/media/**/*.md");
+      const generateMediaJson = () =>
+        src(mediaSrc)
+          .pipe(markdownToJSON(marked))
+          .pipe(
+            merge({
+              concatArrays: true,
+              fileName: "media.json",
+              edit: json => ({ [json.published.split("-").shift()]: [json] })
+            })
+          )
+          .pipe(dest(dataPath));
+
+      task("media-data", generateMediaJson);
+      task("media:watch", cb => {
+        watch(mediaSrc, generateMediaJson);
+        watch(path.resolve(dataPath, "media.json"), series("html"));
+        cb();
+      });
+    },
+    development: {
+      prebuild: ["media-data"],
+      postbuild: ["media:watch"]
+    },
+    production: {
+      prebuild: ["media-data"]
     }
   },
 
