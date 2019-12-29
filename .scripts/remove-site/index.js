22740dee1796559b979f44bbe87b0088cafd6e8c
Aleš Roubíček <ales@roubicek.name>
2019-12-29 15:53:36 +0100

    [Scaffolding] Added ablility to add/remove site CI jobs

--- .scripts/remove-site/index.js
+++ .scripts/remove-site/index.js
@@ -1,16 +1,11 @@
 const commander = require("commander");
 const chalk = require("chalk");
 const fs = require("fs");
+const { pull } = require("lodash");
 const path = require("path");
 const rimraf = require("rimraf");
-
-const Promise = require("bluebird");
-const writeFile = Promise.promisify(fs.writeFile);
-const readFile = Promise.promisify(fs.readFile);
-
-/**re
- * CONSTS
- */
+const YAML = require("yaml");
+const { strOptions } = require("yaml/types");
 
 const VERSION = "0.0.1";
 
@@ -32,7 +27,6 @@ const IS_VERBOSE = !!program.verbose;
 /*
  * Commander End
  ************************************************************************/
-
 
 /**
  * Verbose logger
@@ -85,28 +79,24 @@ if (typeof SITE_DIR_NAME === "undefined" || !SITE_DIR_NAME) {
 /**
  * @return {Promise}
  */
-function removePackageJsonScripts() {
+function removePackageJsonScripts(name) {
   const packageJsonPath = path.resolve("./package.json");
   Logger.log();
   Logger.log("Removing package.json scripts");
   Logger.log();
 
-  return readFile(packageJsonPath, "utf8").then(data => {
+  return fs.promises.readFile(packageJsonPath, "utf8").then(data => {
     const packageConfig = JSON.parse(data);
 
-    delete packageConfig.scripts[
-      `start:${SITE_DIR_NAME}`
-    ];
-    delete packageConfig.scripts[
-      `build:${SITE_DIR_NAME}`
-    ];
-    delete packageConfig.scripts[
-      `test:broken-links:${SITE_DIR_NAME}`
-    ];
+    delete packageConfig.scripts[`start:${name}`];
+    delete packageConfig.scripts[`build:${name}`];
+    delete packageConfig.scripts[`test:broken-links:${name}`];
 
-    const packageConfigStrigified = JSON.stringify(packageConfig, null, 2);
-
-    return writeFile(packageJsonPath, packageConfigStrigified, "utf8");
+    return fs.promises.writeFile(
+      packageJsonPath,
+      JSON.stringify(packageConfig, null, 2),
+      "utf8"
+    );
   });
 }
 
@@ -116,16 +106,37 @@ function removeSiteFromIaaC(name) {
   Logger.log(`Removing website ${name} from websites.json manifest`);
   Logger.log();
 
-  return readFile(filePath, "utf8").then(data => {
+  return fs.promises.readFile(filePath, "utf8").then(data => {
     const websites = JSON.parse(data);
     // remove website configuration
     delete websites[name];
-    return writeFile(filePath, JSON.stringify(websites, null, 2), "utf8");
+    return fs.promises.writeFile(
+      filePath,
+      JSON.stringify(websites, null, 2),
+      "utf8"
+    );
+  });
+}
+
+function removeSiteFromCI(name) {
+  Logger.log();
+  Logger.log(`Removing website ${name} from .circleci/config.yml build script`);
+  Logger.log();
+  const siteName = name.replace(/\./g, "-");
+  const filePath = path.resolve("./.circleci/config.yml");
+  return fs.promises.readFile(filePath, "utf-8").then(data => {
+    const yamlData = YAML.parseDocument(data);
+    const jobs = yamlData.getIn(["workflows", "build_and_deploy", "jobs"]);
+    const siteJobs = jobs.items
+      .filter(x => x.has("deploy_site") || x.has("test_site"))
+      .filter(x => x.items[0].value.get("site_name") === siteName);
+    pull(jobs.items, ...siteJobs);
+    strOptions.fold.lineWidth = 0;
+    return fs.promises.writeFile(filePath, yamlData.toString(), "utf-8");
   });
 }
 
 function deleteSiteDirectory(root) {
-
   Logger.log();
   Logger.log(`Removing website ${root} directory`);
   Logger.log();
@@ -136,15 +147,12 @@ function deleteSiteDirectory(root) {
 function remove(name) {
   const root = path.resolve(name);
 
-    deleteSiteDirectory(root);
-    removeSiteFromIaaC(name)
-    .then(() => removePackageJsonScripts())
+  deleteSiteDirectory(root);
+  removeSiteFromIaaC(name)
+    .then(() => removePackageJsonScripts(name))
+    .then(() => removeSiteFromCI(name))
     .then(() => {
-      console.log(
-        `Site ${chalk.green(
-          `${SITE_DIR_NAME}`
-        )} succesfully removed`
-      );
+      console.log(`Site ${chalk.green(`${name}`)} succesfully removed`);
       console.log();
       console.log();
       console.log("-----------------------------------------------");
