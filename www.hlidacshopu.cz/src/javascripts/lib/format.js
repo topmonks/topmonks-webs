6f4983b4a87a07b7a00b19b235fe2afcb1fb72b6
Aleš Roubíček <ales@roubicek.name>
2020-10-17 08:24:29 +0200

    [www.hlidacshopu.cz] Remove site

--- www.hlidacshopu.cz/src/javascripts/lib/format.js
+++
@@ -1,9 +0,0 @@
-export const formatMoney = x =>
-  x
-    .toLocaleString("cs", {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2
-    })
-    .replace(/,00/g, ",-");
-export const formatPercents = x =>
-  `${Math.round(100 * x).toLocaleString("cs")} %`;
