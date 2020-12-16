11f8aa96159d349be430aa52ba31cc34ab4eb4c6
Václav Slavík <vaclav.slavik@topmonks.com>
2020-12-16 11:28:49 +0100

    [cbx.cz] new site introduced

---
+++ cbx.cz/config/task-config.js
@@ -0,0 +1,26 @@
+const createSharedTaskConfig = require("../../shared/config/createSharedTaskConfig");
+const pathConfig = require("./path-config.json");
+
+const config = createSharedTaskConfig(__dirname, {
+  images: false,
+  cloudinary: false,
+  fonts: false,
+  static: true,
+  svgSprite: true,
+  javascripts: false,
+  stylesheets: true,
+  workboxBuild: false,
+  html: true,
+
+  browserSync: {
+    server: {
+      baseDir: pathConfig.dest
+    }
+  },
+
+  production: {
+    rev: true
+  }
+});
+
+module.exports = config;
