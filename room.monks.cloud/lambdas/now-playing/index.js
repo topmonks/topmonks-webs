e3193bb8ac3dc4e97f3870ef72a1f80d134499d8
Aleš Roubíček <ales@roubicek.name>
2021-01-25 19:57:29 +0100

    [room.monks.cloud] Added CORS

--- room.monks.cloud/lambdas/now-playing/index.js
+++ room.monks.cloud/lambdas/now-playing/index.js
@@ -2,12 +2,46 @@
 /** @typedef { import("@pulumi/awsx/apigateway").Response } APIGatewayProxyResult */
 
 /**
+ * @param {Record<string, any>} body
+ * @param {Record<string, boolean | number | string>} [headers]
+ * @returns {APIGatewayProxyResult}
+ */
+function response(body, headers) {
+  return {
+    statusCode: 200,
+    body: typeof body === "string" ? body : JSON.stringify(body),
+    headers
+  };
+}
+
+/**
+ * @callback ResponseTransformer
+ * @param {APIGatewayProxyResult} in
+ * @returns {APIGatewayProxyResult}
+ */
+/**
+ * @param {string | string[]} methods
+ * @param {string} [origin]
+ * @return {ResponseTransformer}
+ */
+function withCORS(methods, origin = "*") {
+  const allowMethods = Array.isArray(methods) ? methods.join(",") : methods;
+  return x => ({
+    ...x,
+    headers: {
+      ...x.headers,
+      "Access-Control-Allow-Origin": origin,
+      "Access-Control-Allow-Methods": allowMethods,
+      "Access-Control-Allow-Headers":
+        "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token"
+    }
+  });
+}
+
+/**
  * @param {APIGatewayProxyEvent} event
  * @returns {Promise.<APIGatewayProxyResult>}
  */
 export async function handler(event) {
-  return {
-    statusCode: 200,
-    body: "Nutin"
-  };
+  return withCORS(["GET"])(response("Nutin"));
 }
