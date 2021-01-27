219ba0081a493b902392ed829186ce4cb68a5679
Aleš Roubíček <ales@roubicek.name>
2021-01-27 05:17:31 +0100

    [room.monks.cloud] Extracted HTTP module

---
+++ room.monks.cloud/lambdas/http.mjs
@@ -0,0 +1,46 @@
+/**
+ * @param {Record<string, any> | string} body
+ * @param {Record<string, boolean | number | string>} [headers]
+ * @returns {APIGatewayProxyResult}
+ */
+export function response(body, headers) {
+  return {
+    statusCode: 200,
+    body: typeof body === "string" ? body : JSON.stringify(body),
+    headers
+  };
+}
+/**
+ * @param {Record<string, any>} body
+ * @returns {APIGatewayProxyResult}
+ */
+export function notFound(body = { error: "Data not found" }) {
+  return {
+    statusCode: 404,
+    body: JSON.stringify(body)
+  };
+}
+
+/**
+ * @callback ResponseTransformer
+ * @param {APIGatewayProxyResult} in
+ * @returns {APIGatewayProxyResult}
+ */
+/**
+ * @param {string | string[]} methods
+ * @param {string} [origin]
+ * @return {ResponseTransformer}
+ */
+export function withCORS(methods, origin = "*") {
+  const allowMethods = Array.isArray(methods) ? methods.join(",") : methods;
+  return x => ({
+    ...x,
+    headers: {
+      ...x.headers,
+      "Access-Control-Allow-Origin": origin,
+      "Access-Control-Allow-Methods": allowMethods,
+      "Access-Control-Allow-Headers":
+        "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token"
+    }
+  });
+}
--- room.monks.cloud/lambdas/now-playing/index.js
+++ room.monks.cloud/lambdas/now-playing/index.js
@@ -1,56 +1,9 @@
 import * as cheerio from "cheerio";
 import fetch from "node-fetch";
+import { response, notFound, withCORS } from "../http.mjs";
 
 /** @typedef { import("@pulumi/awsx/apigateway").Request } APIGatewayProxyEvent */
 /** @typedef { import("@pulumi/awsx/apigateway").Response } APIGatewayProxyResult */
-
-/**
- * @param {Record<string, any> | string} body
- * @param {Record<string, boolean | number | string>} [headers]
- * @returns {APIGatewayProxyResult}
- */
-function response(body, headers) {
-  return {
-    statusCode: 200,
-    body: typeof body === "string" ? body : JSON.stringify(body),
-    headers
-  };
-}
-
-/**
- * @param {Record<string, any>} body
- * @returns {APIGatewayProxyResult}
- */
-export function notFound(body = { error: "Data not found" }) {
-  return {
-    statusCode: 404,
-    body: JSON.stringify(body)
-  };
-}
-
-/**
- * @callback ResponseTransformer
- * @param {APIGatewayProxyResult} in
- * @returns {APIGatewayProxyResult}
- */
-/**
- * @param {string | string[]} methods
- * @param {string} [origin]
- * @return {ResponseTransformer}
- */
-function withCORS(methods, origin = "*") {
-  const allowMethods = Array.isArray(methods) ? methods.join(",") : methods;
-  return x => ({
-    ...x,
-    headers: {
-      ...x.headers,
-      "Access-Control-Allow-Origin": origin,
-      "Access-Control-Allow-Methods": allowMethods,
-      "Access-Control-Allow-Headers":
-        "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token"
-    }
-  });
-}
 
 /**
  * @param {APIGatewayProxyEvent} event
@@ -61,7 +14,7 @@ export async function handler(event) {
   const resp = await fetch("https://serato.com/playlists/Alessio_Busta/live");
   const html = await resp.text();
   const $ = cheerio.load(html);
-  let $playlist = $(".playlist-trackname");
+  const $playlist = $(".playlist-trackname");
   if (!$playlist.length) {
     return withCORS(["GET"])(notFound("Not playing"));
   }
