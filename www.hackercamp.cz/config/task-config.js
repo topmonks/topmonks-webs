b357bb6570ee7e64ff48aebab8e9372af0b229f6
Aleš Roubíček <ales@roubicek.name>
2021-04-22 14:49:53 +0200

    [www.hackercamp.cz] Import images collection

--- www.hackercamp.cz/config/task-config.js
+++ www.hackercamp.cz/config/task-config.js
@@ -11,7 +11,7 @@ const config = createSharedTaskConfig(__dirname, {
   stylesheets: true,
   workboxBuild: false,
   html: {
-    collections: []
+    collections: ["images"]
   },
 
   browserSync: {
