a9bb90d1085bc82dfbeb28ef5e262ddea6cf8d32
Aleš Roubíček <ales@roubicek.name>
2021-05-12 08:50:55 +0200

    Simplify lambda builder, bump deps, cleanup

---
+++ lambda-builder.ts
@@ -0,0 +1,23 @@
+import * as pulumi from "@pulumi/pulumi";
+import { buildSync } from "esbuild";
+
+function build(entrypoint: string, minify: boolean) {
+  const result = buildSync({
+    bundle: true,
+    minify,
+    charset: "utf8",
+    platform: "node",
+    target: "node14",
+    mainFields: ["module", "main"],
+    external: ["aws-sdk"],
+    entryPoints: [entrypoint],
+    write: false
+  });
+  return result?.outputFiles?.[0].text ?? "";
+}
+
+export function buildCodeAsset(entrypoint: string, minify = false) {
+  return new pulumi.asset.AssetArchive({
+    "index.js": new pulumi.asset.StringAsset(build(entrypoint, minify))
+  });
+}
--- room.monks.cloud/infra/index.ts
+++ room.monks.cloud/infra/index.ts
@@ -1,76 +1,65 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
-// @ts-ignore
-import * as lambdaBuilder from "../../lambda-builder.js";
+import * as lambdaBuilder from "../../lambda-builder";
 import * as path from "path";
 import { CustomDomainDistribution } from "@topmonks/pulumi-aws/apigateway";
 
-export async function init() {
-  const builder = await lambdaBuilder.init();
-  const buildAssets = (fileName: string) =>
-    builder.buildCodeAsset(path.join(__dirname, "..", "lambdas", fileName));
+const buildAssets = (fileName: string) =>
+  lambdaBuilder.buildCodeAsset(path.join(__dirname, "..", "lambdas", fileName));
 
-  const defaultLambdaRole = new aws.iam.Role("monksroom-default-lambda-role", {
-    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(
-      aws.iam.Principals.LambdaPrincipal
-    )
-  });
+const defaultLambdaRole = new aws.iam.Role("monksroom-default-lambda-role", {
+  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal(
+    aws.iam.Principals.LambdaPrincipal
+  )
+});
 
-  new aws.iam.RolePolicyAttachment(
-    "monksroom-lambda-basic-execution-attachment",
+new aws.iam.RolePolicyAttachment(
+  "monksroom-lambda-basic-execution-attachment",
+  {
+    policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole,
+    role: defaultLambdaRole
+  }
+);
+
+export const api = new awsx.apigateway.API("monksroom-api", {
+  stageName: "v1",
+  restApiArgs: {
+    description: "Monksroom API"
+  },
+  routes: [
     {
-      policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole,
-      role: defaultLambdaRole
-    }
-  );
-
-  const api = new awsx.apigateway.API("monksroom-api", {
-    stageName: "v1",
-    restApiArgs: {
-      description: "Monksroom API"
+      path: "/now-playing",
+      method: "GET",
+      eventHandler: new aws.lambda.Function("monksroom-now-playing", {
+        publish: true,
+        runtime: aws.lambda.Runtime.NodeJS14dX,
+        role: defaultLambdaRole.arn,
+        handler: "index.handler",
+        code: buildAssets("now-playing/index.js")
+      })
     },
-    routes: [
-      {
-        path: "/now-playing",
-        method: "GET",
-        eventHandler: new aws.lambda.Function("monksroom-now-playing", {
-          publish: true,
-          runtime: aws.lambda.Runtime.NodeJS14dX,
-          role: defaultLambdaRole.arn,
-          handler: "index.handler",
-          code: buildAssets("now-playing/index.js")
-        })
-      },
-      {
-        path: "/beatport",
-        method: "GET",
-        eventHandler: new aws.lambda.Function("monksroom-beatport", {
-          publish: true,
-          runtime: aws.lambda.Runtime.NodeJS14dX,
-          role: defaultLambdaRole.arn,
-          handler: "index.handler",
-          code: buildAssets("beatport/index.js"),
-          timeout: 20
-        })
-      }
-    ]
-  });
-
-  const apiDistribution = new CustomDomainDistribution(
-    "hlidac-shopu-api",
     {
-      gateway: api,
-      domainName: "room-api.monks.cloud",
-      basePath: "v1"
-    },
-    { dependsOn: [api] }
-  );
-  return {
-    api,
-    apiDistribution,
-    stop() {
-      return builder.stop();
+      path: "/beatport",
+      method: "GET",
+      eventHandler: new aws.lambda.Function("monksroom-beatport", {
+        publish: true,
+        runtime: aws.lambda.Runtime.NodeJS14dX,
+        role: defaultLambdaRole.arn,
+        handler: "index.handler",
+        code: buildAssets("beatport/index.js"),
+        timeout: 20
+      })
     }
-  };
-}
+  ]
+});
+
+export const apiDistribution = new CustomDomainDistribution(
+  "hlidac-shopu-api",
+  {
+    gateway: api,
+    domainName: "room-api.monks.cloud",
+    basePath: "v1"
+  },
+  { dependsOn: [api] }
+);
