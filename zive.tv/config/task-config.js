0edb8348cd4977a6730661b6414aa68456cc2333
Aleš Roubíček <ales@roubicek.name>
2021-06-04 10:46:07 +0200

    [zive.tv] added stream helper Countdown

--- zive.tv/config/task-config.js
+++ zive.tv/config/task-config.js
@@ -1,5 +1,8 @@
+const esbuild = require("gulp-esbuild");
+const mode = require("gulp-mode")();
 const createSharedTaskConfig = require("../../shared/config/createSharedTaskConfig");
 const pathConfig = require("./path-config.json");
+const projectPath = require("@topmonks/blendid/gulpfile.js/lib/projectPath.js");
 
 const config = createSharedTaskConfig(__dirname, {
   images: true,
@@ -19,6 +22,45 @@ const config = createSharedTaskConfig(__dirname, {
       }
     }
   },
+
+  esbuild: {
+    extensions: ["js", "mjs"],
+    options: {
+      bundle: true,
+      splitting: true,
+      minify: mode.production(),
+      sourcemap: true,
+      format: "esm",
+      platform: "browser",
+      target: ["es2019"],
+      charset: "utf8"
+    }
+  },
+
+  additionalTasks: {
+    initialize(gulp, pathConfig, taskConfig) {
+      const { src, task, dest } = gulp;
+      const paths = {
+        src: projectPath(pathConfig.src, pathConfig.esbuild.src, "*.js"),
+        dest: projectPath(pathConfig.dest, pathConfig.esbuild.dest)
+      };
+      task("esbuild-prod", () =>
+        src(paths.src)
+          .pipe(esbuild(taskConfig.esbuild.options))
+          .pipe(dest(paths.dest))
+      );
+      const gulpEsbuild = esbuild.createGulpEsbuild({ incremental: true });
+      task("esbuild", () =>
+        src(paths.src)
+          .pipe(gulpEsbuild(taskConfig.esbuild.options))
+          .pipe(dest(paths.dest))
+      );
+    },
+    development: { code: ["esbuild"] },
+    production: { code: ["esbuild-prod"] }
+  },
+
+  watch: { tasks: ["esbuild"] },
 
   browserSync: {
     server: {
